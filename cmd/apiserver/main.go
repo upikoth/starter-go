567f596b61bce4accd39ef072e47a14e07c82f26
upikoth <upikoth@gmail.com>
2023-04-23 21:47:55 +0300

    fix: убрана ошибка если нет .env файла в проекте

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -8,10 +8,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	config := apiserver.NewConfig()
 	server := apiserver.New(config)

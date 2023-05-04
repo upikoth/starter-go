b130e055385ee07d8eefc0494685e3ee6ace3730
upikoth <upikoth@gmail.com>
2023-05-04 22:08:03 +0300

    chore(hotreload): добавлен hotreload в dev режиме

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,1 +1,4 @@
-go run cmd/apiserver/main.go
+# nodemon обеспечивает live reload сервера
+# для запуска скрипта он должен быть установлен глобально
+# npm install -g nodemon
+nodemon --exec go run cmd/apiserver/main.go --signal SIGTERM
